caf986f12d912d226276c3c77fb7c88d1ee645d8
Mikayla Bruce <mkla.w.b@gmail.com>
2022-03-14 18:17:10 -0700

    thought model worked on

--- models/Thought.js
+++ models/Thought.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema
 
 const reactionSchema =  new mongoose.Schema({
-
+thoughts : [
+    {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }
+]
 })
 
 const thoughtSchema = new mongoose.Schema({
