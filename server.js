42bf7bfce1a691a3223145ddb068bc75dd6ea372
Mikayla Bruce <mkla.w.b@gmail.com>
2022-03-15 11:37:12 -0700

    server js file complete and index.js file added to controllers foulder

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require("express");
-const mongoose = require('mongoose');
-const routes = require('./routes')
+const mongoose = require("mongoose");
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({ extended : true }))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes)
+app.use(express.static("public"));
 
-mongoose.connect()
+app.use(routes);
 
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/The-Social-Network",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.listen(PORT, () => console.log(`Now listening at ${PORT}`));
