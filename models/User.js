c6341e773f3e940f319268cd611e9f3903fe8333
Mikayla Bruce <mkla.w.b@gmail.com>
2022-03-16 18:14:57 -0700

    user model. not compelte yet

--- models/User.js
+++ models/User.js
@@ -1,6 +1,34 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    unique: true,
+    // validate: true
+    //     * Must match a valid email address (look into Mongoose's matching validation)
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Thought",
+    },
+  ],
+  //     * Array of `_id` values referencing the `User` model (self-reference)
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
 
 const User = mongoose.model("User", userSchema);
 
