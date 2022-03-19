336f29935b88cabac459ff4ea0d15b2d6a86228c
Mikayla Bruce <mkla.w.b@gmail.com>
2022-03-18 18:08:27 -0700

    user model complete

--- models/User.js
+++ models/User.js
@@ -1,37 +1,45 @@
-const mongoose = require("mongoose");
+const { Schema , model } = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    unique: true,
-    // validate: true
-    //     * Must match a valid email address (look into Mongoose's matching validation)
-  },
-  thoughts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Thought",
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
     },
-  ],
-  //     * Array of `_id` values referencing the `User` model (self-reference)
-  friends: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "User",
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      unique: true,
+      match: /.+\@.+\..+/,
     },
-  ],
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.lenght;
 });
 
 const User = mongoose.model("User", userSchema);
-
-const handleError = (err) => console.error(err);
 
 module.exports = User;
